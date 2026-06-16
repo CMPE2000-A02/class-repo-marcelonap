3cc7774a09a96694c20fadf86acb09dcea5310c7
Marcelo Napoleao Sampaio <73085259+marcelonap@users.noreply.github.com>
2026-06-15 20:26:09 -0600

    Extension Methods demo

--- ComparisonDemo/ComparisonDemo/CNum.cs
+++ ComparisonDemo/ComparisonDemo/CNum.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ComparisonDemo
@@ -9,17 +10,50 @@ namespace ComparisonDemo
   internal class CNum : IComparable
   {
     public int Data { get; set; }
+    public float Ratio { get; set; }
     public string Name { get; set; }
-    public CNum(int n, string name)
+
+    public double Product
     {
+      get
+      {
+        return Data * Ratio;
+      }
+    }
+
+    //public bool OffsetData
+    //{
+    //  set
+    //  {
+    //    if (value)
+    //    {
+    //      Data += 32;
+    //    }
+    //  }
+    //}
+    public CNum(int n, string name) : this()
+    {
+      Console.WriteLine("Running int + string constructor");
       Data = n;
       Name = name;
+    }
+
+    public CNum(float fDecimal) : this(10, "T")
+    {
+      Console.WriteLine("Running float constructors");
+      Ratio = fDecimal;
+    }
+
+    public CNum()
+    {
+      Console.WriteLine("Running default constructors");
+      Random rand = new Random();
+      Ratio = (float)rand.NextDouble();
     }
 
     //Static preferred way to implement custom comparison methos
     public static int AscendingComparison(CNum num1, CNum num2)
     {
-
       return num1.Data.CompareTo(num2.Data);
     }
 
@@ -51,7 +85,7 @@ namespace ComparisonDemo
 
     public override string ToString()
     {
-      return $"Data: {Data} Name: {Name}";
+      return $"Data: {Data} Name: {Name} Product: {Product}";
     }
   }
 }
---
+++ ExtensionsDemo/ExtensionsDemo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtensionsDemo
+{
+  public partial class Form1 : Form
+  {
+
+    List<int> ints = new List<int>();
+    public Form1()
+    {
+      InitializeComponent();
+      for (int i = 1; i <= 10; ++i)
+      {
+        ints.Add(i);
+      }
+      int product = ints.Product();
+      Console.WriteLine($"Product is: {product}");
+    }
+  }
+}
